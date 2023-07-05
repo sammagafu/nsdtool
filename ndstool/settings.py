7a028066e054d46fe73675893372842f1ebcbcc7
Samuel Magafu <hi@sammagafu.com>
2023-07-05 07:15:07 +0300

    fixed database issues

--- ndstool/settings.py
+++ ndstool/settings.py
@@ -99,10 +99,15 @@ WSGI_APPLICATION = 'ndstool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecdtool',
+        'USER': 'ecdroot',
+        'PASSWORD': 'Pediatrics-2020',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
