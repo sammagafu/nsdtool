0b9e5d15997bf050cdd11d1ec27c7492062161ed
Samuel Magafu <hi@sammagafu.com>
2023-07-05 06:22:25 +0300

    Removed request for static files

--- ndstool/urls.py
+++ ndstool/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
@@ -10,4 +8,4 @@ urlpatterns = [
     path('api/v1/category/', include('questioncategory.urls')),
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/', include('djoser.urls.authtoken')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
